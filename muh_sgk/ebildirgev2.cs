8417d7f984ea307ff90b729db2beeeea08aede0a
Rana <Rana@DESKTOP-89G3BRQ>
2021-09-15 21:37:46 +0300

    selenium3

--- muh_sgk/ebildirgev2.cs
+++ muh_sgk/ebildirgev2.cs
@@ -12,8 +12,7 @@ using System.Net;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
-
-
+using System.Collections.ObjectModel;
 
 namespace muh_sgk
 {
@@ -34,12 +33,46 @@ namespace muh_sgk
             IWebDriver driver = new ChromeDriver();
             string v = textBox1.Text.ToString().Trim();
             driver.Navigate().GoToUrl("https://ebildirge.sgk.gov.tr/EBildirgeV2/login/kullaniciIlkKontrollerGiris.action?username=12178849190&isyeri_kod=084&password=32978025&isyeri_sifre=70272627&isyeri_guvenlik=" + v + "");
-            driver.Navigate().GoToUrl("https://ebildirge.sgk.gov.tr/EBildirgeV2/tahakkuk/tahakkukonaylanmisTahakkukDonemSecildi.action?hizmet_yil_ay_index=2&hizmet_yil_ay_index_bitis=10");
+            driver.Navigate().GoToUrl("https://ebildirge.sgk.gov.tr/EBildirgeV2/tahakkuk/tahakkukonaylanmisTahakkukDonemSecildi.action?hizmet_yil_ay_index=2&hizmet_yil_ay_index_bitis=3");
+ 
+            ReadOnlyCollection<IWebElement> tahakkukadet = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr"));
 
-            IReadOnlyCollection<IWebElement> tahakkukadet = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr"));
+
+            for (int i = 3; i < (tahakkukadet.Count)+1; i++)
+            {
 
 
-            listBox1.Items.Add(tahakkukadet.Count);
+
+
+
+                    ReadOnlyCollection<IWebElement> donemay = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[1]"));
+                    ReadOnlyCollection<IWebElement> hizmetay = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[2]"));
+                    ReadOnlyCollection<IWebElement> belgeturu = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[3]"));
+                    ReadOnlyCollection<IWebElement> belgemahiyeti = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[4]"));
+                    ReadOnlyCollection<IWebElement> kanunno = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[5]"));
+                    ReadOnlyCollection<IWebElement> calisan = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[6]"));
+                    ReadOnlyCollection<IWebElement> gun = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[7]"));
+                    ReadOnlyCollection<IWebElement> spek = driver.FindElements(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[8]"));
+
+                    IWebElement pdf = driver.FindElement(By.XPath("//*[@id=\"contentContainer\"]/div/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[" + i + "]/td[10]/div/a[2]"));
+
+                    
+                    listBox1.Items.Add(donemay.First().Text).ToString();
+                    listBox1.Items.Add(hizmetay.First().Text).ToString();
+                    listBox1.Items.Add(belgeturu.First().Text).ToString();
+                    listBox1.Items.Add(belgemahiyeti.First().Text).ToString();
+                    listBox1.Items.Add(kanunno.First().Text).ToString();
+                    listBox1.Items.Add(calisan.First().Text).ToString();
+                    listBox1.Items.Add(gun.First().Text).ToString();
+                    listBox1.Items.Add(spek.First().Text).ToString();
+                pdf.Click();
+                    
+                    
+
+                
+
+            }
+           
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
